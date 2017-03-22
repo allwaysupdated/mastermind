2dd10232054558fb45ae2073764770dde761f956
Allwaysupdated <jacksonstar3@gmail.com>
2017-03-22 10:09:18 +0100

    fixed the game
    burnout 3 babbbyyyyyyyyyyyyy

--- mastermind2.rb
+++ mastermind2.rb
@@ -1,39 +1,75 @@
-running = true
-puts "Enter difficulty:"
-  difficulty = gets.to_i
-difficulty
-answer = [rand(difficulty).to_s,rand(difficulty).to_s,rand(difficulty).to_s,rand(difficulty).to_s]
-p answer #4debug
-input = []#Array.new
-#input = [gets.chomp.to_s, gets.chomp.to_s, gets.chomp.to_s, gets.chomp.to_s]
-
-progress = ["-", "-", "-", "-"]
-wrong = ["o"]
-right = ["x"]
-times = 0
 
 
-
-  input = [gets.chomp.to_s, gets.chomp.to_s, gets.chomp.to_s, gets.chomp.to_s]
-i = 0
-while running == true
-
-  if input[i] == answer[i]
-    progress.delete_at(i)
-    progress.insert(i,"x")
-    p input
+def randomnumbers(difficulty:)
+  answer =[]
+  i = 0
+  while i<4
+    answer << rand(difficulty).to_s
     i += 1
-    if i == 4
-      i = 0
-    end
-  else
-    progress.delete_at(i)
-    progress.insert(i, "o")
-    i += 1
-    p i
-    if i == 4
-      i = 0
+  end
+    return answer
+end
+
+
+def guessinggame(answer:)
+  running = true
+  i = 0
+  progress = ["-", "-", "-", "-"]
+  endgame = ["x","x","x","x"]
+  puts "
+
+
+
+
+                                                  ENTER GUESS (" + answer.length.to_s + " )NUMBERS"
+  while progress != endgame
+    input = gets.chomp.split("")
+    input[1.to_i]
+    checker = 0
+    while checker <= 4
+      if input[i] == answer[i]
+        progress.delete_at(i)
+        progress.insert(i,"x")
+        i += 1
+        system("cls")
+        puts "
+                                                PLACE IS: " + (i+1).to_s
+                            puts "
+                                                PROGRESS IS: " + progress.to_s
+                              puts "            LAST GUESS:" + input.to_s
+
+        checker += 1
+        if i == 4
+        i = 0
+        end
+      else
+        progress.delete_at(i)
+        progress.insert(i, "o")
+        i += 1
+        system("cls")
+        puts "
+                                                    PLACE IS: " + (i+1).to_s
+                            puts "
+                                                    PROGRESS IS: " + progress.to_s
+                                  puts "            LAST GUESS:" + input.to_s
+
+        checker += 1
+        if i == 4
+        i = 0
+        end
+      end
     end
   end
 
+  if progress == endgame
+    puts "AYY lmao"
+    abort
+  end
 end
+system("cls")
+puts "
+
+
+                                                                                                          ENTER DIFFICULTY"
+answer = randomnumbers(difficulty:gets.to_i)
+p guessinggame(answer: answer)
--- testbench.rb
+++ testbench.rb
@@ -1,11 +1,40 @@
-running = true
+#
 
-list = []
+def randmnumbers(length:)
+  list =[]
 
-list = [gets.chomp]; p list
-#list.insert(0,gets.chomp+gets.chomp+gets.chomp+gets.chomp);p list
+  i = 0
+  while i<length
+    list << rand(50)
+    i += 1
+  end
 
-for list in [1,2,3,] do 
-  p list
-  puts "aymao"
+  return list
 end
+
+
+
+def sorting(list:)
+    progress = 0
+    index = 0
+    i = list.length
+    while progress <= i
+      if list[index] > list[index+1] |
+          oldvalue = list[index+1]
+            newvalue = list[index]
+              list[index] = oldvalue
+                list[index+1] = newvalue
+                  index+=1
+                   progress += 1
+      elsif list[index] > list[index+1]  |# progress +=1
+        oldvalue = list[index+1]
+          newvalue = list[index]
+            list[index] = oldvalue
+              list[index+1] = newvalue
+      end
+    end
+    return list
+end
+
+list = randmnumbers(length:5)
+p sorting(list: list)
